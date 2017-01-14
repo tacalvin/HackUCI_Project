cbdf2986aeb8aeb97e221538437f2dd5e35543e6
jjkjiang <jerryjiekaijiang@gmail.com>
2017-01-14 03:04:44 -0800

    Preference fragment pre-test phase

---
+++ app/src/main/java/com/example/paula/securityapp/PreferenceFragment.java
@@ -0,0 +1,55 @@
+package com.example.paula.securityapp;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.support.v4.app.Fragment;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link PreferenceFragment.OnFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link PreferenceFragment#} factory method to
+ * create an instance of this fragment.
+ */
+public class PreferenceFragment extends PreferenceFragmentCompat {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private EditTextPreference distanceEditTextPreference;
+    private EditTextPreference displayEditTextPreference;
+
+    //private OnFragmentInteractionListener mListener;
+
+    public PreferenceFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        // Indicate here the XML resource you created above that holds the preferences
+        setPreferencesFromResource(R.xml.preferences, rootKey);
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnFragmentInteractionListener {
+        // TODO: Update argument type and name
+        void onFragmentInteraction(Uri uri);
+    }
+}
